de60df235989368422dfea2db4115924177e7da2
Adam ? <adamznow@gmail.com>
2019-05-29 12:51:56 -0700

    Now only splits .assets files instead of literally all of my files
    Splitter accepts many parameters now

--- splitter.py
+++ splitter.py
@@ -1,16 +1,18 @@
 import argparse
 import os
+import glob
 
 SPLIT_SIZE = 1024 * 1024
 
 def split(mainAssetName):
+    if '.assests' not in mainAssetName:
+        return
     with open(mainAssetName, 'rb') as fs:
-        fs.seek(0)
         index = 0
         while len(fs.peek()) > 0:
             with open(mainAssetName + '.split' + str(index), 'wb') as f:
                 f.write(fs.read(SPLIT_SIZE))
-            print("Wrote: " + mainAssetName + '.split' + str(index))
+            # print("Wrote: " + mainAssetName + '.split' + str(index))
             index += 1
 
 def combine(mainAssetPath):
@@ -24,19 +26,22 @@ def combine(mainAssetPath):
                 if f.startswith(name) and f != name and ".split" in f:
                     with open(os.path.join(d, f), 'rb') as fr:
                         fs.write(fr.read())
-                    print("Read: " + os.path.join(d, f))
+                    # print("Read: " + os.path.join(d, f))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Handles splitting to and from .split* files.")
-    parser.add_argument("path", type=str, help="The path to the .asset to read from or write to.")
+    parser.add_argument("path", type=str, help="The files to read from or write to.", nargs='+')
     parser.add_argument("mode", type=int, help="The mode to use. Choose 0 for splitting, 1 for combining.", default=1)
 
     args = parser.parse_args()
 
     if args.mode == 1:
-        combine(args.path)
+        for p in args.path:
+            combine(p)
     elif args.mode == 0:
-        split(args.path)
+        for p in args.path:
+            split(p)
     else:
-        print("Invalid mode!")
+        raise Exception("Must provide a valid mode! [0, 1]")
+        # print("Invalid mode!")
